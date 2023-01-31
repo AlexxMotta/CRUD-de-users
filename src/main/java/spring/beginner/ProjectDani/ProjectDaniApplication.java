8a99646169b60f86e5508782135c6095e4b90c3c
Taylher <alexmota@alu.ufc.br>
2023-01-30 21:30:24 -0300

    first-commit

---
+++ src/main/java/spring/beginner/ProjectDani/ProjectDaniApplication.java
@@ -0,0 +1,13 @@
+package spring.beginner.ProjectDani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDaniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/beginner/ProjectDani/ProjectDaniApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.beginner.ProjectDani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
