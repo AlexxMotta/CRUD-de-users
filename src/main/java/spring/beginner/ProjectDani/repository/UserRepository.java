cba3c7b1c18ca1210c710737ea4f330767cdeb18
AlexxMotta <alexmota@alu.ufc.br>
2023-02-07 10:34:34 -0300

    adicionando o findByName no repository

--- src/main/java/spring/beginner/ProjectDani/repository/UserRepository.java
+++ src/main/java/spring/beginner/ProjectDani/repository/UserRepository.java
@@ -10,4 +10,11 @@ public interface UserRepository extends JpaRepository<User,Long>{
     
     @Query("SELECT u from User u where u.id >= :id")
     public List<User> findAllMoreThan(@Param("id") Long id);
+
+    //greater than - maior que
+    public List<User> findByIdGreaterThan(Long id);
+
+    //o codigo abaixo retorna erro caso mais de um user tem o mesmo name
+    //um solução no JS seria usar o .first() mas aqui é java então ...
+    public User findByName(String name);
 }
